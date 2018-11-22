8ada4e51875eddba0aab7a683008b77b030cd7c0
elliott <e.l.warren@pgr.reading.ac.uk>
2018-11-22 18:01:25 +0000

    added some extra plotting of individual cases

--- scripts/ceil_comparison_plotting.py
+++ scripts/ceil_comparison_plotting.py
@@ -5,7 +5,9 @@ Created by Elliott Warren Thurs 08 Nov 2018
 """
 
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from scipy.stats import spearmanr
 from scipy.stats import pearsonr
 import datetime as dt
@@ -28,15 +30,16 @@ if __name__ == '__main__':
     # --- User changes
 
     # correlate in height or in time?
-    corr_type = 'time'
+    corr_type = 'height'
 
-    #main_ceil_name = 'CL31-A_IMU'
-    #main_ceil_name = 'CL31-B_RGS'
-    #main_ceil_name = 'CL31-C_MR'
-    #main_ceil_name = 'CL31-D_SWT'
-    #main_ceil_name = 'CL31-E_NK'
+    ceil_list = ['CL31-A_IMU', 'CL31-B_RGS', 'CL31-C_MR', 'CL31-D_SWT', 'CL31-E_NK']
+    #ceil_list = ['CL31-A_IMU']
+    #ceil_list = ['CL31-B_RGS']
+    #ceil_list = ['CL31-C_MR']
+    #ceil_list = ['CL31-D_SWT']
+    #ceil_list = ['CL31-E_NK']
 
-    for main_ceil_name in ['CL31-A_IMU', 'CL31-B_RGS', 'CL31-C_MR', 'CL31-D_SWT', 'CL31-E_NK']:
+    for main_ceil_name in ceil_list:
 
         # ------------------
 
@@ -77,7 +80,7 @@ if __name__ == '__main__':
         # save info?
         savestr = main_ceil_name + '_' + corr_type + '_statistics.npy'
 
-        print 'main ceilometer: ' + corr_type + '_' +main_ceil_name
+        print 'main ceilometer: ' + corr_type + '_' + main_ceil_name
 
         # ==============================================================================
         # Read data
@@ -100,6 +103,9 @@ if __name__ == '__main__':
         # ==============================================================================
         # plot data
         # ==============================================================================
+
+        # ---------------------------------
+        # median and IQR corr plot (all cases together)
 
         # plotting details
         if corr_type == 'time':
@@ -138,3 +144,43 @@ if __name__ == '__main__':
             ax.xaxis.set_major_formatter(DateFormatter('%H:%M'))
         plt.suptitle(main_ceil_name+ '; ' + str(len(daystrList)) + ' days; 15sec')
         plt.savefig(savedir + main_ceil_name +'_spearmanr_'+corr_type+'.png')
+
+        # # ---------------------------------
+        # # corr in height - split up days. One plot per pairing. Each case as a seperate line
+        #
+        # # plotting details
+        # if corr_type == 'time':
+        #     x_axis = time_match
+        #     x_label = 'time [HH:MM]'
+        # elif corr_type == 'height':
+        #     x_axis = heights[main_ceil_name]
+        #     x_label = 'height [m]'
+        #
+        # case_num = len(days_iterate) # number of days
+        # # colours = cm.coolwarm(np.linspace(0, 1, case_num))
+        # colours = cm.rainbow(np.linspace(0, 1, case_num))
+        # c_range = np.linspace(0, case_num, 1)
+        # norm = mpl.colors.Normalize(vmin=0, vmax=case_num)
+        #
+        # for paired_site_i in paired_sites:
+        #
+        #     fig = plt.figure()
+        #     ax = plt.gca()
+        #
+        #     corr_rs = statistics[paired_site_i]['corr_rs']
+        #
+        #     for i in range(statistics[paired_site_i]['corr_rs'].shape[1]):
+        #         idx = np.array([any(np.isfinite(row)) for row in corr_rs])
+        #
+        #         plt.plot(x_axis[idx], corr_rs[idx, i], '-', color=colours[i], label=days_iterate[i].strftime('%d/%m/%Y'))
+        #
+        #     # plt.xlim([time_match[0], time_match[-1]])
+        #     plt.ylabel('Spearman r')
+        #     plt.xlabel(x_label)
+        #     plt.ylim([0.0, 1.05])
+        #     plt.legend(loc=1)
+        #     if corr_type == 'time':
+        #         ax.xaxis.set_major_formatter(DateFormatter('%H:%M'))
+        #     plt.suptitle(main_ceil_name + ' & ' + paired_site_i +'; ' + str(len(daystrList)) + ' days; 15sec')
+        #     plt.savefig(savedir + 'sep_paired/' + main_ceil_name + '_' + paired_site_i +'_spearmanr_' + corr_type + '.png')
+        #     plt.close()
