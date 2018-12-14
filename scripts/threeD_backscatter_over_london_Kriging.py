d830f81867dc9cc6afa2ea13aa435d81ac828656
elliott <e.l.warren@pgr.reading.ac.uk>
2018-12-14 17:11:50 +0000

    exact copy of 3D_backscatter_over_london_Kriging.py. Renamed as it could  not be imported as a library, because it started with a number.

--- scripts/threeD_backscatter_over_london_Kriging.py
+++ scripts/threeD_backscatter_over_london_Kriging.py
@@ -1,8 +1,7 @@
 """
-Script to create and plot 3D modelled backscatter over London. Saves the 3D field in a numpy array for post processing
-statistics.
+Script to carry out Kriging on 3D modelled backscatter over London.
 
-Created by Elliott Warren Fri 23 Nov 2018
+Created by Elliott Warren Fri 14 Dec 2018
 """
 
 import numpy as np
@@ -20,6 +19,9 @@ import ceilUtils.ceilUtils as ceil
 from forward_operator import FOUtils as FO
 from forward_operator import FOconstants as FOcon
 
+import sys
+sys.path.append('C:/Users/Elliott/Documents/PhD Reading/PhD Research/Aerosol Backscatter/improveNetworks/scripts')
+from 3D_backscatter_over_london import calculate_corner_locations, rotate_lon_lat_2D
 
 def calculate_corner_locations(coord):
 
@@ -86,6 +88,78 @@ def calculate_corner_locations(coord):
 
     return corner
 
+def rotate_lon_lat_2D(longitude, latitude, corner_locs=False):
+
+    """
+    Create 2D array of lon and lats in rotated space (WGS84)
+
+    :param mod_data:
+    :keyword corner_locs (bool): return lon and lat corner locations so (0,0) is bottom left corner
+            of bottom left box
+    :return: rotlon2D, rotlat2D (2D arrays): 2D arrays of rotate lon and latitudes (rot. to WSG84)
+    """
+
+    # duplicate the single column array but number of rows in latitude
+    # rotlat2D needs to be transposed as rows need to be latitude, and columns longitude.
+    # np.transpose() does transpose in the correct direction:
+    # compare mod_all_data['latitude'][n] with rotlat2D[n,:]
+    rotlon2D = np.array([longitude] * latitude.shape[0])
+    rotlat2D = np.transpose(np.array([latitude] * longitude.shape[0]))
+
+    # Get model transformation object (ll) to unrotate the model data later
+    if model_type == 'UKV':
+        rotpole = (iris.coord_systems.RotatedGeogCS(37.5, 177.5, ellipsoid=iris.coord_systems.GeogCS(
+            6371229.0))).as_cartopy_crs()  # rot grid
+        rotpole2 = ccrs.RotatedPole(pole_longitude=177.5, pole_latitude=37.5)
+        ll = ccrs.Geodetic() # to normal grid
+    else:
+        raise ValueError('model_type not set as UKV!')
+
+    # calculate corners of grid, from their centre positions, of unrotated data
+    # ---------------------
+    if corner_locs == True:
+        rotlat2D = calculate_corner_locations(rotlat2D)
+        rotlat2D = calculate_corner_locations(rotlon2D)
+
+    # 3D array with a slice with nothing but 0s...
+    normalgrid = ll.transform_points(rotpole, rotlon2D, rotlat2D)
+    lons = normalgrid[:, :, 0]
+    lats = normalgrid[:, :, 1]
+    # [:, :, 2] always seems to be 0.0 ... not sure what it is meant to be... land mask maybe...
+
+    # test to see if the functions above have made the corner lat and lons properly
+    # test_create_corners(rotlat2D, rotlon2D, corner_lats, corner_lons)
+
+    return lons, lats
+
+def test_create_corners(rotlat2D, rotlon2D, corner_lats, corner_lons):
+    """ quick plotting to see whether the functions within the script have created the corner lat and lon properly.
+    Original and extrap/interp data together."""
+
+    # copy and pasted out of main functions. Put back if this function is to be used.
+    # # 3D array with a slice with nothing but 0s...
+    # normalgrid = ll.transform_points(rotpole, rotlon2D, rotlat2D)
+    # lons_orig = normalgrid[:, :, 0]
+    # lats_orig = normalgrid[:, :, 1]
+    # # [:, :, 2] always seems to be 0.0 ... not sure what it is meant to be... land mask maybe...
+    #
+    # # 3D array with a slice with nothing but 0s...
+    # normalgrid = ll.transform_points(rotpole, corner_lons, corner_lats)
+    # lons = normalgrid[:, :, 0]
+    # lats = normalgrid[:, :, 1]
+
+    plt.figure()
+    plt.scatter(rotlat2D, rotlon2D, color='red')
+    plt.scatter(corner_lats, corner_lons, color='blue')
+    # plt.scatter(a, d, color='green')
+    # plt.scatter(lats, lons, color='red')
+    # plt.scatter(lats_orig, lons_orig, color='blue')
+    plt.figure()
+    plt.pcolormesh(corner_lats, vmin=np.nanmin(corner_lats), vmax=np.nanmax(corner_lats))
+    plt.colorbar()
+
+    return
+
 if __name__ == '__main__':
 
     # ==============================================================================
@@ -135,62 +209,19 @@ if __name__ == '__main__':
     # calculate the 3D backscatter field across London
     mod_data = FO.mod_site_extract_calc_3D(day, modDatadir, model_type, res, 905, allvars=True)
 
-    # create 2D array of lon and lats in rotated space
-    # duplicate the single column array but number of rows in latitude
-    # rotlat2D needs to be transposed as rows need to be latitude, and columns longitude.
-    # np.transpose() does transpose in the correct direction:
-    # compare mod_all_data['latitude'][n] with rotlat2D[n,:]
-    rotlon2D = np.array([mod_data['longitude']] * mod_data['latitude'].shape[0])
-    rotlat2D = np.transpose(np.array([mod_data['latitude']] * mod_data['longitude'].shape[0]))
-
-    #spec_mesh = np.meshgrid(mod_data['latitude'], mod_data['longitude'])
-
-    # calculate corners of grid, from their centre positions
-
-    # ---------------------
-
-    corner_lats = calculate_corner_locations(rotlat2D)
-    corner_lons = calculate_corner_locations(rotlon2D)
-
-    # unrotate the model data
-    if model_type == 'UKV':
-        rotpole = (iris.coord_systems.RotatedGeogCS(37.5, 177.5, ellipsoid=iris.coord_systems.GeogCS(
-            6371229.0))).as_cartopy_crs()  # rot grid
-        rotpole2 = ccrs.RotatedPole(pole_longitude=177.5, pole_latitude=37.5)
-        ll = ccrs.Geodetic()  # normal grid
-    else:
-        raise ValueError('model_type not set as UKV!')
-
-    # 3D array with a slice with nothing but 0s...
-    normalgrid = ll.transform_points(rotpole, rotlon2D, rotlat2D)
-    lons_orig = normalgrid[:, :, 0]
-    lats_orig = normalgrid[:, :, 1]
-    # [:, :, 2] always seems to be 0.0 ... not sure what it is meant to be... land mask maybe...
-
-    # 3D array with a slice with nothing but 0s...
-    normalgrid = ll.transform_points(rotpole, corner_lons, corner_lats)
-    lons = normalgrid[:, :, 0]
-    lats = normalgrid[:, :, 1]
-
-    plt.figure()
-    plt.scatter(rotlat2D, rotlon2D, color='red')
-    plt.scatter(corner_lats, corner_lons, color='blue')
-    #plt.scatter(a, d, color='green')
-    #plt.scatter(lats, lons, color='red')
-    # plt.scatter(lats_orig, lons_orig, color='blue')
-    plt.figure()
-    plt.pcolormesh(corner_lats, vmin=np.nanmin(corner_lats), vmax=np.nanmax(corner_lats))
-    plt.colorbar()
+    # rotate the lon and lats onto a normal geodetic grid (WGS84)
+    lons, lats = rotate_lon_lat_2D(mod_data['longitude'], mod_data['latitude'], model_type)
 
     # ==============================================================================
     # Plotting
     # ==============================================================================
 
     # fast plot - need to convert lon and lats from centre points to corners for pcolormesh()
+    height_idx = 6
     for hr_idx, hr in enumerate(mod_data['time']):
         fig, ax = plt.subplots(1, 1, figsize=(6, 4.4))
 
-        mesh = plt.pcolormesh(lons, lats, mod_data['bsc_attenuated'][hr_idx, 6, :, :],
+        mesh = plt.pcolormesh(lons, lats, mod_data['bsc_attenuated'][hr_idx, height_idx, :, :],
                        norm=LogNorm(vmin=1e-7, vmax=1e-5), cmap=cm.get_cmap('jet'))
 
         # plot each ceilometer location
@@ -202,7 +233,7 @@ if __name__ == '__main__':
         ax.set_xlabel(r'$Longitude$')
         ax.set_ylabel(r'$Latitude$')
         plt.colorbar(mesh)
-        plt.suptitle(hr.strftime('%Y-%m-%d_%H') + ' backscatter')
+        plt.suptitle(hr.strftime('%Y-%m-%d_%H') + ' beta; height='+str(mod_data['level_height'][height_idx])+'m')
         savename = hr.strftime('%Y-%m-%d_%H') + '_beta.png'
         plt.savefig(savedir + savename)
         plt.close(fig)
