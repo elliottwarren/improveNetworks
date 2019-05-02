9ac8f27505de093185fb94844c68ad3137c95e29
elliott <e.l.warren@pgr.reading.ac.uk>
2019-05-02 10:24:35 +0100

    works wit murk aer too.

--- scripts/plt_surf_frac.py
+++ scripts/plt_surf_frac.py
@@ -7,7 +7,6 @@ Edited by Elliott Warren: Thurs 25 Apr 2018
 """
 
 import iris
-import matplotlib
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import matplotlib.colors as mcols
@@ -26,6 +25,14 @@ from os.path import join, exists
 #import komodo.util.timekeeper as kt
 
 def main():
+
+    # what to plot? Surface types of orography? (Come in different files)
+    # data_to_plot = 'surface type'
+    # data_to_plot = 'orography'
+    data_to_plot = 'murk_aer'
+
+    model_type = 'UKV'
+
     # veg_pseudo_levels=1,2,3,4,5,7,8,9,601,602
     SurfTypeD={1:'Broadleaf trees', 2:'Needleleaf trees', 3:'C3 (temperate) grass',
                4:'C4 (tropical) grass', 5:'shrubs',
@@ -36,35 +43,83 @@ def main():
     llabels=('Broadleaf trees','Needleleaf trees','C3 (temperate) grass',
              'C4 (tropical) grass','Shrubs', 
              'Inland water', 'Bare Soil', 'Ice', 
-             'Urban canyon',  'Urban roof' )
+             'Urban canyon',  'Urban roof')
 
     maindir = 'C:/Users/Elliott/Documents/PhD Reading/PhD Research/Aerosol Backscatter/improveNetworks/'
     datadir = maindir + 'data/UKV/'
+    npydatadir = maindir + '/data/npy/'
     savedir = maindir + 'figures/model_ancillaries/'
 
     # # Cristina's
     # UK_lat_constraint = iris.Constraint(grid_latitude=lambda cell: -2.0 < cell < -0.5)
     # UK_lon_constraint = iris.Constraint(grid_longitude=lambda cell: 360.0 < cell < 362.5)
     # UKV London domain
-    UK_lat_constraint = iris.Constraint(grid_latitude=lambda cell: -1.2326999 < cell < -0.7737)
-    UK_lon_constraint = iris.Constraint(grid_longitude=lambda cell: 361.21997 < cell < 361.73297)
 
-    temp_cmap = plt.get_cmap('BuPu')
-    aspectRatio = 1.8571428571428572 # from my other plots
+    aspectRatio = 1.8571428571428572  # from my other plots
 
-    # Pick color map
-    surf_frac=iris.load_cube(datadir + 'ukv_surf_frac_types.nc',constraint=UK_lat_constraint & UK_lon_constraint)
-    for i,iid in enumerate(surf_frac.coord('pseudo_level').points):
-        if iid not in [4,9]: # ignore ice and tropical grass
-            plt.subplots(1,1, figsize=(4.5 * aspectRatio, 4.5))
-            im=iplt.pcolormesh(surf_frac[i],cmap=temp_cmap)
-            plt.colorbar(im,orientation='vertical')
-            plt.gca().coastlines('10m')
-            plt.title(SurfTypeD[iid])
-            plt.savefig(savedir + 'UKV_'+SurfTypeD[iid]+'.png')
+    if data_to_plot == 'surface type':
+
+        temp_cmap = plt.get_cmap('BuPu')
+
+        # Pick color map
+        surf_frac=iris.load_cube(datadir + 'ukv_surf_frac_types.nc',constraint=UK_lat_constraint & UK_lon_constraint)
+        for i,iid in enumerate(surf_frac.coord('pseudo_level').points):
+            if iid not in [4, 9]: # ignore ice and tropical grass
+                plt.subplots(1,1, figsize=(4.5 * aspectRatio, 4.5))
+                im=iplt.pcolormesh(surf_frac[i],cmap=temp_cmap)
+                plt.colorbar(im,orientation='vertical')
+                plt.gca().coastlines('10m')
+                plt.title(SurfTypeD[iid])
+                plt.savefig(savedir + 'UKV_'+SurfTypeD[iid]+'.png')
+                plt.close()
+
+    elif data_to_plot == 'orography': # orography
+
+        if model_type == 'UKV':
+            spacing = 0.0135  # spacing between lons and lats in rotated space
+            UK_lat_constraint = iris.Constraint(
+                grid_latitude=lambda cell: -1.2326999 - spacing <= cell <= -0.7737 + spacing)
+            UK_lon_constraint = iris.Constraint(grid_longitude=lambda cell: 361.21997 <= cell <= 361.73297 + spacing)
+            orog = iris.load_cube(datadir + 'UKV_orography.nc', constraint=UK_lat_constraint & UK_lon_constraint)
+
+        temp_cmap = plt.get_cmap('terrain')
+        aspectRatio = 1.8571428571428572  # from my other plots
+
+        # Pick color map
+        orog = iris.load_cube(datadir + 'UKV_orography.nc', constraint=UK_lat_constraint & UK_lon_constraint)
+        plt.subplots(1, 1, figsize=(4.5 * aspectRatio, 4.5))
+        im = iplt.pcolormesh(orog, cmap=temp_cmap)
+        plt.colorbar(im, orientation='vertical')
+        plt.gca().coastlines('10m')
+        plt.title('orography')
+        plt.savefig(savedir + 'UKV_orography.png')
+        plt.close()
+
+    elif data_to_plot == 'murk_aer':
+
+        murksavedir = savedir + 'murk_aer/'
+        if os.path.exists(murksavedir) == False:
+            os.mkdir(murksavedir)
+
+        temp_cmap = plt.get_cmap('jet')
+
+        # load data
+        murk_aer = np.load(npydatadir + model_type + '_murk_ancillaries.npy').flatten()[0]
+        for height_idx in np.arange(8):
+            #height_idx = 10
+            height = murk_aer.coord('level_height').points[height_idx]
+            month_idx = 6
+            murk_data = murk_aer.data[month_idx, height_idx, :, :]
+            plt.subplots(1, 1, figsize=(4.5 * aspectRatio, 4.5))
+            im = plt.pcolormesh(murk_data, cmap=temp_cmap, vmin=0, vmax=0.18)
+            plt.colorbar(im, orientation='vertical')
+            #plt.gca().coastlines('10m')
+            plt.title('murk_aer')
+            savename = 'UKV_murk_aer_'+str(month_idx+1)+'_'+str(height)+'m.png'
+            plt.savefig(murksavedir + savename)
             plt.close()
 
-    plt.show()
+
     
 
 if __name__ == '__main__':
