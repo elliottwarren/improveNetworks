3c332ac25b78f9d923cd07efd170876a3b532a9b
elliott <e.l.warren@pgr.reading.ac.uk>
2019-05-02 10:24:04 +0100

    update to cluster code. Plots murk aer and orog

--- scripts/cluster_analysis_after_PCA.py
+++ scripts/cluster_analysis_after_PCA.py
@@ -32,9 +32,12 @@ sys.path.append('C:/Users/Elliott/Documents/PhD Reading/PhD Research/Aerosol Bac
 
 import numpy as np
 import iris
-#import matplotlib
-#matplotlib.use('Agg')
+import matplotlib as mpl
+#mpl.use('Agg')
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.pyplot as plt
+from scipy import stats
+from ellUtils import ellUtils as eu
 
 import os
 from sklearn.cluster import AgglomerativeClustering
@@ -58,29 +61,34 @@ if __name__ == '__main__':
     # subsampled?
     #pcsubsample = 'full'
     #pcsubsample = '11-18_hr_range'
-    pcsubsample = 'daytime'
-    #pcsubsample = 'nighttime'
+    #pcsubsample = 'daytime'
+    pcsubsample = 'nighttime'
 
     # cluster type - to match the AgglomerativeClustering function and used in savename
     linkage_type = 'ward'
 
     # number of clusters
-    n_clusters = 5
+    n_clusters = 7
 
     # ------------------
 
     # which modelled data to read in
     model_type = 'UKV'
 
+    # ancillary type to read, calc and plot
+    ancil_type = 'murk_aer'
+
     # directories
     maindir = 'C:/Users/Elliott/Documents/PhD Reading/PhD Research/Aerosol Backscatter/improveNetworks/'
     datadir = maindir + 'data/'
+    npydatadir = datadir + 'npy/'
     ukvdatadir = maindir + 'data/UKV/'
     ceilDatadir = datadir + 'L1/'
     modDatadir = datadir + model_type + '/'
     pcsubsampledir = maindir + 'figures/model_runs/PCA/'+pcsubsample+'/'
     savedir = pcsubsampledir + data_var+'/'
-    clustersavedir = savedir + '/cluster_analysis/'
+    clustersavedir = savedir + 'cluster_analysis/'
+    histsavedir = clustersavedir + 'histograms/'
     npysavedir = datadir + 'npy/PCA/'
 
     # ==============================================================================
@@ -88,7 +96,7 @@ if __name__ == '__main__':
     # ==============================================================================
 
     # make directory paths for the output figures
-    for dir_i in [clustersavedir]:
+    for dir_i in [clustersavedir, histsavedir]:
         if os.path.exists(dir_i) == False:
             os.mkdir(dir_i)
 
@@ -107,7 +115,6 @@ if __name__ == '__main__':
     lats = raw['latitude']
 
     # 3. Orography
-
     # manually checked and lined up against UKV data used in PCA.
     # slight mismatch due to different number precision used in both (hence why spacing is not used to reduce lower
     #   longitude limit below.
@@ -117,6 +124,9 @@ if __name__ == '__main__':
         UK_lon_constraint = iris.Constraint(grid_longitude=lambda cell: 361.21997 <= cell <= 361.73297+spacing)
         orog = iris.load_cube(ukvdatadir + 'UKV_orography.nc', constraint=UK_lat_constraint & UK_lon_constraint)
 
+    # 4. murk ancillaries (shape = month, height, lat, lon)
+    murk_aer = np.load(npydatadir + model_type+'_murk_ancillaries.npy').flatten()[0]
+
     # ==============================================================================
     # Process data
     # ==============================================================================
@@ -125,10 +135,26 @@ if __name__ == '__main__':
     #linkage_type = ''
     cluster = AgglomerativeClustering(n_clusters=n_clusters, affinity='euclidean', linkage=linkage_type)
     cluster.fit_predict(data)
-    cluster_groups = cluster.labels_
+    # +1 so first group = 1, not 0. Also it produces a 1D array, flattened Fortran style (column-wise)
+    cluster_groups = cluster.labels_ +1
+    group_numbers = np.unique(cluster_groups)
 
-    # split orography into groups based on clusters
-    orog_groups = [orog]
+    # # split orography into groups based on clusters
+    # # Flatten in fortran style to match the clustering code above(column wise instead of default row-wise('C'))
+    # orog_groups = [orog.data.flatten('F')[cluster_groups == i] for i in group_numbers]
+    # # Kruskal-Wallis test (non-parametric equivalent of ANOVA)
+    # kw_s, kw_p = stats.kruskal(*orog_groups)
+    if ancil_type == 'murk_aer':
+        height = 0
+        month_idx = 0
+        ancil_data = murk_aer.data[month_idx, height, :, :]
+
+
+    # split the ancillary into groups based on the cluster groups
+    ancil_groups = [ancil_data.flatten('F')[cluster_groups == i] for i in group_numbers]
+
+    # Kruskal-Wallis test (non-parametric equivalent of ANOVA)
+    kw_s, kw_p = stats.kruskal(*ancil_groups)
 
     # plot
     lat_shape = int(lats.shape[0])
@@ -139,13 +165,52 @@ if __name__ == '__main__':
     groups_reshape = np.transpose(
         np.vstack([cluster_groups[n:n + lat_shape] for n in np.arange(0, X_shape, lat_shape)]))  # 1225
 
+    # ==============================================================================
+    # Plotting
+    # ==============================================================================
+
+    # Looks complicated... tries to overlap histograms
+    # n, x, rectangles = plt.hist(orog_groups, bins=20, histtype='stepfilled',alpha=0.8)
+    # colors = [i[0].get_facecolor() for i in rectangles] # copy colours from first histogram plot
+    # _, _, rectangles2 = plt.hist(orog_groups, bins=20, histtype='step', color=colors, alpha=1)
+    # [i[0].set_linewidth(2) for i in rectangles2]
+
+    vmin=np.percentile(np.hstack(ancil_groups), 5)
+    vmax=np.percentile(np.hstack(ancil_groups), 95)
+    step=(vmax-vmin)/20.0
+
+    fig, axs = plt.subplots(n_clusters, 1, sharex=True, sharey=True)
+    # hist plot each group of orography onto each axis
+    for i, ax_i in enumerate(axs):
+
+        # ax_i.hist(ancil_groups[i], bins=np.arange(0, 220, 10),
+        #              histtype='stepfilled',alpha=0.8)
+        ax_i.hist(ancil_groups[i], bins=np.arange(vmin, vmax, step),
+                     histtype='stepfilled',alpha=0.8)
+        eu.add_at(ax_i, str(i+1), loc=5) # +1 to have groups start from 1 not 0.
+
+    plt.suptitle('K-W test p=%3.2f' % kw_p)
+
+    savename = data_var+'_'+pcsubsample+'_'+ancil_type+'_'+str(n_clusters)+'clusters.png'
+    plt.savefig(histsavedir + savename)
+    plt.close()
+
+    #2. Plot cluster analysis groups
     #fig, ax = plt.subplots(1, 1, figsize=(4.0, 4.0))  # * aspectRatio
     fig = plt.figure(figsize=(6.0, 6.0*0.7*aspectRatio))  # * aspectRatio
     ax = fig.add_subplot(111, aspect=aspectRatio)
 
-    pcmesh = ax.pcolormesh(lons, lats, groups_reshape)
+    #cmap, norm = eu.discrete_colour_map(0, n_clusters, 1)
+    cmap = plt.get_cmap('jet', n_clusters)
+    norm = mpl.colors.BoundaryNorm(np.arange(1,n_clusters+2), cmap.N)
+
+    pcmesh = ax.pcolormesh(lons, lats, groups_reshape, cmap=cmap, norm=norm)
     ax.set_xlabel(r'$Longitude$')
     ax.set_ylabel(r'$Latitude$')
+    the_divider = make_axes_locatable(ax)
+    color_axis = the_divider.append_axes("right", size="5%", pad=0.1)
+    cbar=plt.colorbar(pcmesh, ticks=group_numbers, cax=color_axis)
+    #cbar=plt.colorbar(pcmesh, cax=color_axis)
     plt.tight_layout()
 
     # plot each ceilometer location
