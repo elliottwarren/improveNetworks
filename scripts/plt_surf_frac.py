c2b42e77bbcb0533dfd26b1ae637a93832d0a088
elliottwarren <e.l.warren@pgr.reading.ac.uk>
2019-07-05 15:18:17 +0100

    Copied orography read function to improve modularity. Cleaned up murk plotting so it is better with LM data

--- scripts/plt_surf_frac.py
+++ scripts/plt_surf_frac.py
@@ -19,9 +19,50 @@ import iris.plot as iplt
 import iris.palette
 import numpy as np
 import os
-#import komodo.util.timekeeper as kt
 
 from threeD_backscatter_over_london import rotate_lon_lat_2D
+
+from threeD_backscatter_over_london_PCA import read_orography
+
+def load_plot_surface_type(datadir, UK_lat_constraint, UK_lon_constraint, savedir, model_type):
+    """
+    PLot UKV land cover surface type - needs some reworking
+    :param datadir:
+    :param UK_lat_constraint:
+    :param UK_lon_constraint:
+    :param savedir:
+    :param model_type:
+    :param SurfTypeD:
+    :return:
+    """
+
+    # veg_pseudo_levels=1,2,3,4,5,7,8,9,601,602
+    SurfTypeD={1:'Broadleaf trees', 2:'Needleleaf trees', 3:'C3 (temperate) grass',
+               4:'C4 (tropical) grass', 5:'shrubs',
+               7:'Inland water', 8:'Bare Soil', 9:'Ice',
+               601:'Urban canyon', 602: 'Urban roof' }
+
+
+    llabels=('Broadleaf trees','Needleleaf trees','C3 (temperate) grass',
+             'C4 (tropical) grass','Shrubs',
+             'Inland water', 'Bare Soil', 'Ice',
+             'Urban canyon',  'Urban roof')
+
+    temp_cmap = plt.get_cmap('BuPu')
+
+    # Pick color map
+    surf_frac = iris.load_cube(datadir + 'ukv_surf_frac_types.nc', constraint=UK_lat_constraint & UK_lon_constraint)
+    for i, iid in enumerate(surf_frac.coord('pseudo_level').points):
+        if iid not in [4, 9]:  # ignore ice and tropical grass
+            plt.subplots(1, 1, figsize=(5, 4.5))
+            im = iplt.pcolormesh(surf_frac[i], cmap=temp_cmap)
+            plt.colorbar(im, orientation='vertical')
+            plt.gca().coastlines('10m')
+            plt.title(SurfTypeD[iid])
+            plt.savefig(savedir + model_type + '_' + SurfTypeD[iid] + '.png')
+            plt.close()
+
+    return
 
 def read_murk_aer(datadir, model_type):
 
@@ -55,20 +96,8 @@ if __name__ == '__main__':
     #data_to_plot = 'orography'
     data_to_plot = 'murk_aer'
 
-    model_type = 'UKV'
-    #model_type = 'LM'
-
-    # veg_pseudo_levels=1,2,3,4,5,7,8,9,601,602
-    SurfTypeD={1:'Broadleaf trees', 2:'Needleleaf trees', 3:'C3 (temperate) grass',
-               4:'C4 (tropical) grass', 5:'shrubs',
-               7:'Inland water', 8:'Bare Soil', 9:'Ice',
-               601:'Urban canyon', 602: 'Urban roof' }
-
-
-    llabels=('Broadleaf trees','Needleleaf trees','C3 (temperate) grass',
-             'C4 (tropical) grass','Shrubs', 
-             'Inland water', 'Bare Soil', 'Ice', 
-             'Urban canyon',  'Urban roof')
+    #model_type = 'UKV'
+    model_type = 'LM'
 
     if model_type == 'UKV':
         maindir = 'C:/Users/Elliott/Documents/PhD Reading/PhD Research/Aerosol Backscatter/improveNetworks/'
@@ -101,27 +130,20 @@ if __name__ == '__main__':
 
     if data_to_plot == 'surface type':
 
-        temp_cmap = plt.get_cmap('BuPu')
+        load_plot_surface_type(datadir, UK_lat_constraint, UK_lon_constraint, savedir, model_type, SurfTypeD)
 
-        # Pick color map
-        surf_frac=iris.load_cube(datadir + 'ukv_surf_frac_types.nc',constraint=UK_lat_constraint & UK_lon_constraint)
-        for i,iid in enumerate(surf_frac.coord('pseudo_level').points):
-            if iid not in [4, 9]: # ignore ice and tropical grass
-                plt.subplots(1,1, figsize=(5, 4.5))
-                im=iplt.pcolormesh(surf_frac[i],cmap=temp_cmap)
-                plt.colorbar(im,orientation='vertical')
-                plt.gca().coastlines('10m')
-                plt.title(SurfTypeD[iid])
-                plt.savefig(savedir + model_type+'_'+SurfTypeD[iid]+'.png')
-                plt.close()
 
     elif data_to_plot == 'orography': # orography
 
-        if model_type == 'UKV':
-            orog = iris.load_cube(datadir + 'UKV_orography.nc', orog_con)
-        elif model_type == 'LM':
-            # new_data = iris.load_cube(datadir + '20181022T2100Z_London_charts', 'surface_altitude', constraint=orog_con)
-            orog = iris.load_cube(datadir + '20181022T2100Z_London_charts', orog_con) # orog_con
+        # if model_type == 'UKV':
+        #     orog = iris.load_cube(datadir + 'UKV_orography.nc', orog_con)
+        # elif model_type == 'LM':
+        # new_data = iris.load_cube(datadir + '20181022T2100Z_London_charts', 'surface_altitude', constraint=orog_con)
+        #orog = iris.load_cube(datadir + '20181022T2100Z_London_charts', orog_con) # orog_con
+
+        # Read in orography
+        orog = read_orography(model_type)
+
 
         # rotate lon and lat back to normal
         orog_rot_lats = orog.coord('grid_latitude').points
@@ -147,7 +169,7 @@ if __name__ == '__main__':
         temp_cmap = plt.get_cmap('jet')
 
         # load data
-        murk_aer = read_murk_aer(model_type)
+        murk_aer = read_murk_aer(datadir, model_type)
 
         rot_lats = murk_aer.coord('grid_latitude').points
         rot_lons = murk_aer.coord('grid_longitude').points# - 360.0 - removing 360 has no real effect on rotation
@@ -155,12 +177,14 @@ if __name__ == '__main__':
         # aspectRatio = float(lons.shape[0]) / float(lats.shape[1])
         aspectRatio = float(lons.shape[1]) / float(lats.shape[0])
 
-        #height_idx = 4# 10
-        #height = murk_aer.coord('level_height').points[height_idx]
-        month_idx = 7
-        # murk_data = murk_aer.data[month_idx, height_idx, :, :]
-        murk_data = murk_aer.data
-        # plt.subplots(1, 1, figsize=(4.5 * aspectRatio, 4.5))
+        # extract murk data - UKV already subsampled in time and height, LM isn't
+        if model_type == 'UKV':
+            murk_aer_data = murk_aer.data
+        elif model_type == 'LM':
+            height_idx = 4
+            month_idx = 7
+            murk_data = murk_aer.data[month_idx, height_idx, :, :]
+
         fig, ax = plt.subplots(1, 1, figsize=(6 * aspectRatio, 5))
         temp_cmap = plt.get_cmap('jet')
         im = plt.pcolormesh(lons, lats, murk_data, cmap=temp_cmap, vmin=0, vmax=0.18)
@@ -174,6 +198,8 @@ if __name__ == '__main__':
         cax = divider.append_axes("right", size="5%", pad=-0.1)
         plt.colorbar(im, cax=cax, format='%1.3f')
 
+        ax.xaxis.set_ticks(np.arange(-0.5, 0.3, 0.1)) # paper plot
+        ax.yaxis.set_ticks(np.arange(51.3, 51.6, 0.1)) # paper plot
         ax.set_aspect(aspectRatio, adjustable=None)
         #plt.tight_layout()
         plt.subplots_adjust(bottom=0.1, top=0.9, left=0.1)
